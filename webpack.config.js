68bbb3b820f7759320d5c6cd50843a97a815e2de
Crystal.L <crystal.l0815@gmail.com>
2019-03-28 21:35:38 -0700

    Set up webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/public');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
