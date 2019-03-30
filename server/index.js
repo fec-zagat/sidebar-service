77ddfc655906d03bf679aa2d496693c6ae4bfa0c
Crystal.L <crystal.l0815@gmail.com>
2019-03-29 18:00:36 -0700

    Implement GET request to get data from database

--- server/index.js
+++ server/index.js
@@ -2,16 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
+const { find } = require('../database/Restaurant');
 
 const app = express();
 const port = 3333;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 
 
-app.get('/r/side-bar', (req, res) => res.send('Hello World!'));
+app.get('/sidebar', (req, res) => {
+  find((err, results) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.send(results);
+    }
+  });
+});
 
 app.listen(port, () => console.log(`LISTENING ON ${port}`));
