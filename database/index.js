2b1363f59d797f0405a0a14523e8c120c4f4fbd5
Crystal.L <crystal.l0815@gmail.com>
2019-03-29 17:59:47 -0700

    Implement function to check database connection

--- database/index.js
+++ database/index.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 
-const mongoURL = 'mongodb://localhost/r';
+const mongoURL = 'mongodb://localhost/restaurants';
 
-const db = mongoose.connect(mongoURL);
+mongoose.connect(mongoURL, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('CONNECTED');
+});
+
+db.on('error', () => console.log('ERRRORRRR'));
 
 module.exports = db;
