51c5da9add233eda0e83f6e425d4ab1d9ac4641a
Crystal.L <crystal.l0815@gmail.com>
2019-04-05 16:30:09 -0700

    Create FaceTime component

---
+++ client/src/components/FaceTime.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const FaceTime = () => (
+  <div>
+    <span>
+      Open FaceTime?
+    </span>
+    <span>
+      Always open these types of links in the associated app
+    </span>
+    <button type="button">Cancel</button>
+    <button type="button">Open FaceTime</button>
+  </div>
+);
+
+
+export default FaceTime;
