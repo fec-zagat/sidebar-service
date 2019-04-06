e79425366c73addc58fd36f6aa0dc7bf335de322
Crystal.L <crystal.l0815@gmail.com>
2019-04-06 11:07:47 -0700

    Add google map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Map extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-    };
+    this.apiKey = 'AIzaSyAHVxGlmHpg2eeLdURo8ZjJSVLRc_7672k';
+    this.mapURL = (latitude, longtitude) => `https://www.google.com/maps/embed/v1/place?q=${latitude},${longtitude}&key=${this.apiKey}`;
   }
 
   render() {
+    const { address } = this.props;
+    const { latitude, longtitude } = address;
     return (
       <div>
-          MAP POP OUT
+        <iframe
+          title="googleMap"
+          height="450"
+          frameBorder="0"
+          className="mapIframe"
+          src={this.mapURL(latitude, longtitude)}
+        />
       </div>
     );
   }
 }
 
 export default Map;
+
+Map.propTypes = {
+  address: PropTypes.object.isRequired,
+};
