6f68f7f633b4b225e081370bdbfe6a7e0243f45b
Crystal.L <crystal.l0815@gmail.com>
2019-04-08 17:05:15 -0700

    Add iframe to map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,42 +1,30 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-import PropTypes from 'prop-types';
 import GOOGLEMAP_API_KEY from '../config/googleMap';
 
-const mapStyles = {
-  width: '100%',
-  height: '100%',
-};
-
-export class MapContainer extends Component {
+class Map extends Component {
   constructor(props) {
     super(props);
 
     this.apiKey = GOOGLEMAP_API_KEY;
-    this.mapURL = (latitude, longtitude) => `https://www.google.com/maps/embed/v1/place?q=${latitude},${longtitude}&key=${this.apiKey}`;
+    this.mapUrl = (lat, long) => `https://www.google.com/maps/embed/v1/place?q=${lat},${long}&key=${this.apiKey}`;
   }
 
   render() {
-    const { address, google } = this.props;
+    const { address } = this.props;
     const { latitude, longtitude } = address;
     return (
-      <Map
-        google={google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-          lat: latitude,
-          lng: longtitude,
-        }}
-      />
+      <div>
+        <iframe
+          title="googleMap"
+          width="380"
+          height="300"
+          frameBorder="0"
+          className="mapIframe"
+          src={this.mapUrl(latitude, longtitude)}
+        />
+      </div>
     );
   }
 }
 
-Map.propTypes = {
-  address: PropTypes.object.isRequired,
-};
-
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAHVxGlmHpg2eeLdURo8ZjJSVLRc_7672k',
-})(Map);
+export default Map;
